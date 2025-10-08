563444cfd0c3f2b95845958640f29536452cfee4
Marisela Vasquez <vasquem2@oregonstate.edu>
2025-10-08 13:54:15 -0700

    Updated pdf path and image imported into ProjectData

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -1,39 +1,48 @@
 // Data File to hold array of project objects 
 
+
+import timeCalImg from './images/projectImgs/timeCal.jpg';
+import langtonImg from './images/projectImgs/langtonAnt.png';
+import hasamiImg  from './images/projectImgs/HasamiShogiGame.png';
+import spmImg     from './images/projectImgs/StringPrimMac.png';
+import pdfAutoImg from './images/projectImgs/PdfAutomation.png';
+
+const base = import.meta.env.BASE_URL;
+
 const projectData = [
     {
     id: 1,
     title: 'clockTimeCal',
-    image: 'src/components/images/projectImgs/timeCal.jpg',
+    image: timeCalImg,
     link: 'https://timeworkcal.vercel.app',
     description: 'A calculator that helps hourly employees figure out their exact clock-out time based on start time and required work hours.'
     },
     {
     id: 2,
     title: "Langton's Ant",
-    image: 'src/components/images/projectImgs/langtonAnt.png',
+    image: langtonImg,
     link: 'https://github.com/alesiram/161-Final-Project-LangtonAnt',
     description: 'A grid-based simulation of Langton’s Ant using OOP, with customizable board size, steps, and ant position.'
     },
     {
     id: 3,
     title: 'Hasami Shogi Game ',
-    image: 'src/components/images/projectImgs/HasamiShogiGame.png',
+    image: hasamiImg,
     link: 'https://github.com/alesiram/162-Final-Project-HasamiShogi',
     description: 'OOP game class that enforces Variant-1 rules: 9×9 board with algebraic notation, legal-move validation, multi-side custodian captures, turn/state tracking, and captured-piece counts.'
     }, 
     {
     id: 4,
     title: 'String Primitives and Macros',
-    image: 'src/components/images/projectImgs/StringPrimMac.png',
+    image: spmImg,
     link: 'https://github.com/alesiram/271_String_Primitives_and_Macros',
     description: 'Assembly language program (MASM) that reads numbers as strings, validates input, converts to integers, and displays their sum and average.'
     }, 
     {
     id: 5,
     title: 'Form Automation & Dashboard',
-    image: 'src/components/images/projectImgs/PdfAutomation.png',
-    link: 'src/components/PDF_Files/PHQ9_Survey.pdf' ,
+    image: pdfAutoImg,
+    link: '${base}public/PDF_Files/PHQ9_Survey.pdf' ,
     description: 'Built an automated scoring system for the PHQ-9 clinical assessment form using Acrobat JavaScript and SDK scripting. (Open in Adobe Acrobat for full functionality.)'
     }, 
     
